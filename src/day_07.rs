34bf3af01a95f3a9c510c07ce42f828f33ee17c9
Gernot Raudner <gernot.raudner@dynatrace.com>
2022-12-08 10:24:27 +0000

    day 07 complete

--- src/day_07.rs
+++ src/day_07.rs
@@ -7,6 +7,7 @@ pub fn day_07() {
     let tree = read_into_file_tree("./input/day_07.txt");
     let sum = calc_dirs_lt_100k(&tree);
     println!("sum of dirs lt 100k {}", sum);
+    println!("size of smallest eligible dir {}", find_smallest_delete_dir(&tree));
 }
 
 fn calc_dirs_lt_100k(files: &HashMap<String, Rc<RefCell<FileTree>>>) -> usize {
@@ -17,54 +18,76 @@ fn calc_dirs_lt_100k(files: &HashMap<String, Rc<RefCell<FileTree>>>) -> usize {
         .sum()
 }
 
+fn find_smallest_delete_dir(files: &HashMap<String, Rc<RefCell<FileTree>>>) -> usize {
+    let root_size = files.get("/").unwrap().borrow().size;
+    let required_size = 30_000_000 - (70_000_000 - root_size);
+    let mut eligible_dirs: Vec<&Rc<RefCell<FileTree>>> = files.values()
+        .filter(|file| file.borrow().is_dir)
+        .filter(|file| file.borrow().size >= required_size)
+        .collect();
+    eligible_dirs.sort_by_key(|dir| dir.borrow().size);
+    eligible_dirs.first().unwrap().borrow().size
+}
+
+fn command_into_file_tree<'a>((mut paths, mut map): (Vec<&'a str>, HashMap<String, Rc<RefCell<FileTree>>>), command: (&'a str, &'a str, Option<&'a str>)) -> (Vec<&'a str>, HashMap<String, Rc<RefCell<FileTree>>>) {
+    let parent_path = paths.join("/");
+    let parent_rc = map.get(&parent_path).map(Rc::clone);
+    match command {
+        ("$", "cd", Some("/")) => {
+            paths.push("/");
+            let full_path = paths.join("/");
+            let tree = FileTree::new(0, parent_rc);
+            map.insert(full_path, Rc::new(RefCell::new(tree)));
+        },
+        ("$", "cd", Some("..")) => {
+            paths.pop();
+        }
+        ("$", "cd", Some(dir)) => {
+            paths.push(dir);
+        },
+        ("dir", dirname, _) => {
+            let full_path = paths.join("/") + "/" + dirname;
+            let tree = FileTree::new(0, parent_rc);
+            map.insert(full_path, Rc::new(RefCell::new(tree)));
+        },
+        (_, "ls", _) => (),
+        (size, name, _) => {
+            let full_path = paths.join("/") + "/" + name;
+            let tree = FileTree::new(size.parse().unwrap(), parent_rc);
+            map.insert(full_path, Rc::new(RefCell::new(tree)));
+        }
+    }
+    (paths, map)
+}
+
+fn line_to_command(line: &str) -> (&str, &str, Option<&str>) {
+    let mut words = line.split(' ');
+    (words.next().unwrap(), words.next().unwrap(), words.next())
+}
+
 fn read_into_file_tree(path: &str) -> HashMap<String, Rc<RefCell<FileTree>>> {
     let input = fs::read_to_string(path).unwrap();
     let (_, files) = input
         .split('\n')
-        .map(|line| {
-            let mut words = line.split(' ');
-            (words.next().unwrap(), words.next().unwrap(), words.next())
-        })
-        .fold((vec![], HashMap::new()), |(mut paths, mut map), command| {
-            let parent_path = paths.join("/");
-            let parent_rc = map.get(&parent_path).map(|parent| Rc::clone(&parent));
-            match command {
-                ("$", "cd", Some("/")) => {
-                    paths.push("/");
-                    let full_path = paths.join("/");
-                    let tree = FileTree::new(String::from("/"), 0, parent_rc);
-                    map.insert(full_path, Rc::new(RefCell::new(tree))).map(|_| panic!("Path already exists!"));
-                },
-                ("$", "cd", Some("..")) => {
-                    paths.pop();
-                }
-                ("$", "cd", Some(dir)) => {
-                    paths.push(dir);
-                },
-                ("dir", dirname, _) => {
-                    let full_path = paths.join("/") + "/" + dirname;
-                    let tree = FileTree::new(String::from(dirname), 0, parent_rc);
-                    map.insert(full_path, Rc::new(RefCell::new(tree))).map(|_| panic!("Path already exists!"));
-                },
-                (_, "ls", _) => (),
-                (size, name, _) => {
-                    let full_path = paths.join("/") + "/" + name;
-                    let tree = FileTree::new(String::from(name), size.parse().unwrap(), parent_rc);
-                    map.insert(full_path, Rc::new(RefCell::new(tree))).map(|_| panic!("Path already exists!"));
-                }
-            }
-            (paths, map)
-        });
+        .map(line_to_command)
+        .fold((vec![], HashMap::new()), command_into_file_tree);
     files
 }
 #[cfg(test)]
 mod tests {
-    use super::{read_into_file_tree, calc_dirs_lt_100k};
+    use super::{read_into_file_tree, calc_dirs_lt_100k, find_smallest_delete_dir};
 
     #[test]
     fn test_part_1() {
         let tree = read_into_file_tree("./input/day_07.test.txt");
         let sum = calc_dirs_lt_100k(&tree);
         assert_eq!(sum, 95437);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let tree = read_into_file_tree("./input/day_07.test.txt");
+        let smallest = find_smallest_delete_dir(&tree);
+        assert_eq!(smallest, 24933642);
     }
 }
--- src/day_07/file_tree.rs
+++ src/day_07/file_tree.rs
@@ -2,17 +2,16 @@ use std::{cell::{RefCell, RefMut}, rc::{Rc, Weak}, borrow::BorrowMut};
 
 pub struct FileTree {
     parent: Option<Rc<RefCell<FileTree>>>,
-    name: String,
     pub size: usize,
     pub is_dir: bool
 }
 
 impl FileTree {
-    pub fn new(name: String, size: usize, parent: Option<Rc<RefCell<FileTree>>>) -> FileTree {
+    pub fn new(size: usize, parent: Option<Rc<RefCell<FileTree>>>) -> FileTree {
         if let Some(ref parent) = parent {
             parent.as_ref().borrow_mut().add_size(size);
         }
-        FileTree { parent, name, size, is_dir: size == 0 }
+        FileTree { parent, size, is_dir: size == 0 }
     }
     fn add_size(&mut self, size: usize) {
         self.size += size;
@@ -29,14 +28,14 @@ mod tests {
     use super::FileTree;
     #[test]
     fn test_child_adding() {
-        let root = FileTree::new(String::from("/"), 0, None);
+        let root = FileTree::new(0, None);
         let root_rc = Rc::new(RefCell::new(root));
-        FileTree::new(String::from("a"), 5, Some(Rc::clone(&root_rc)));
-        FileTree::new(String::from("b"), 4, Some(Rc::clone(&root_rc)));
-        let child_dir = FileTree::new(String::from("dir"), 0, Some(Rc::clone(&root_rc)));
+        FileTree::new(5, Some(Rc::clone(&root_rc)));
+        FileTree::new(4, Some(Rc::clone(&root_rc)));
+        let child_dir = FileTree::new(0, Some(Rc::clone(&root_rc)));
         assert_eq!(root_rc.borrow().size, 9);
         let sub_rc = Rc::new(RefCell::new(child_dir));
-        FileTree::new(String::from("child"), 3, Some(Rc::clone(&sub_rc)));
+        FileTree::new(3, Some(Rc::clone(&sub_rc)));
         assert_eq!(root_rc.borrow().size, 12);
         assert_eq!(sub_rc.borrow().size, 3);
     }
